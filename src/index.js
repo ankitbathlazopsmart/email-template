c544438cde99cf7b48db039f2db9f096126b087a
ankit Bathla <ankit.bathla@zopsmart.com>
2021-10-25 15:15:29 +0530

    feat: init

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
+window.LoginWidget = class LoginWidget {
+    init(opts) {
+        const pageConfig = opts.pageConfig;
+        if (!pageConfig) {
+            throw new Error("pageConfig must be provided in opts");
+        }
+
+        ReactDOM.render(
+            <App pageConfig={pageConfig} />,
+            document.getElementById("root")
+        );
+    }
+};
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals

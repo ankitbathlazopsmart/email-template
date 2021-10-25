8b1af67a0b531ac9ba04a17be2af393d8c3f938d
ankit Bathla <ankit.bathla@zopsmart.com>
2021-10-25 16:48:51 +0530

    feat: bundle

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-/* eslint-disable react/style-prop-object */
 import "./App.css";
 function App({ pageConfig }) {
     console.log(pageConfig);
@@ -7,7 +6,6 @@ function App({ pageConfig }) {
             <>
                 <center>
                     <table
-                        style='width: 600px;-webkit-text-size-adjust: 100%;-ms-text-size-adjust: 100%;mso-table-lspace: 0pt;mso-table-rspace: 0pt;margin: 0;padding: 0;font-family: "ProximaNova", sans-serif;border-collapse: collapse !important;height: 100% !important;'
                         align="center"
                         border="0"
                         cellpadding="0"
@@ -17,19 +15,13 @@ function App({ pageConfig }) {
                         id="bodyTable"
                     >
                         <tr>
-                            <td
-                                align="center"
-                                valign="top"
-                                id="bodyCell"
-                                style='-webkit-text-size-adjust: 100%;-ms-text-size-adjust: 100%;mso-table-lspace: 0pt;mso-table-rspace: 0pt;margin: 0;padding: 20px;font-family: "ProximaNova", sans-serif;height: 100% !important;'
-                            >
+                            <td align="center" valign="top" id="bodyCell">
                                 <div class="main">
-                                    <p style="text-align: center;-webkit-text-size-adjust: 100%;-ms-text-size-adjust: 100%; margin-bottom: 30px;">
+                                    <p>
                                         <img
                                             src="https://cdn.auth0.com/styleguide/2.0.9/lib/logos/img/badge.png"
                                             width="50"
                                             alt="Your logo goes here"
-                                            style="-ms-interpolation-mode: bicubic;border: 0;height: auto;line-height: 100%;outline: none;text-decoration: none;"
                                         />
                                     </p>
                                     <h1>Welcome to !</h1>
@@ -54,8 +46,8 @@ function App({ pageConfig }) {
                                     <strong></strong>
                                     <br />
                                     <br />
-                                    <hr style="border: 2px solid #EAEEF3; border-bottom: 0; margin: 20px 0;" />
-                                    <p style="text-align: center;color: #A9B3BC;-webkit-text-size-adjust: 100%;-ms-text-size-adjust: 100%;">
+                                    <hr />
+                                    <p>
                                         If you did not make this request, please
                                         contact us by replying to this mail.
                                     </p>

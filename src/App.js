c0bbda4b1863f8ada494ede870464681c3050ea6
ankit Bathla <ankit.bathla@zopsmart.com>
2021-10-25 16:29:33 +0530

    feat:welcome

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import WelcomeTemplate from "./components/welcome_template/welcomeEmail";
 function App({ pageConfig }) {
-    console.log(pageConfig);
+    const { application } = pageConfig;
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <WelcomeTemplate application={application} />
         </div>
     );
 }
